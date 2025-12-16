606da6ca75c0520a1ce312605c94d9dc164505be
aditya-arcot <dev@aarcot.com>
2025-12-16 10:30:26 -0600

    add database session

---
+++ server/app/core/dependencies.py
@@ -0,0 +1,21 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from app.core.config import settings
+
+async_engine = create_async_engine(
+    settings.DATABASE_URL,
+    future=True,
+    echo=settings.ENV != "prod",
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session

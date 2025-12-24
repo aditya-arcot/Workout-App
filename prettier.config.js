b1aca3ba152bbf4b4a6fdb6527af671b7f7101d6
aditya-arcot <dev@aarcot.com>
2025-12-24 10:40:06 -0600

    add tailwind prettier plugin, update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,11 @@ module.exports = {
     trailingComma: 'es5',
     semi: false,
     singleQuote: true,
-    plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-toml'],
+    plugins: [
+        'prettier-plugin-organize-imports',
+        'prettier-plugin-toml',
+        // must be last
+        'prettier-plugin-tailwindcss',
+    ],
+    tailwindStylesheet: './client/src/index.css',
 }

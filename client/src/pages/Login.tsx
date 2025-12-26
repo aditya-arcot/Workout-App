208c3987f23a3b4e0f839997a6c771c0ebd9ac5f
aditya-arcot <dev@aarcot.com>
2025-12-25 19:10:48 -0600

    client - implement login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,64 @@
+import { AuthService } from '@/api/generated'
+import { useSession } from '@/auth/session'
+import type { LocationState } from '@/models/location'
+import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
+
 export function Login() {
-    return <div>Login Page - TODO implement</div>
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const location = useLocation()
+    const state = location.state as LocationState | null
+    const from = state?.from?.pathname ?? '/login'
+
+    const { refresh } = useSession()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            await AuthService.login({ body: { username, password } })
+            await refresh()
+            void navigate(from, { replace: true })
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <form
+            onSubmit={(e) => {
+                void handleSubmit(e)
+            }}
+        >
+            <h1>Login</h1>
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => {
+                    setUsername(e.target.value)
+                }}
+                placeholder="Username"
+                required
+            />
+            <br />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                }}
+                placeholder="Password"
+                required
+            />
+            <br />
+            <button disabled={loading} type="submit">
+                {loading ? 'Logging in…' : 'Login'}
+            </button>
+        </form>
+    )
 }

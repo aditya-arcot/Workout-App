b534b7f99d00e020c2be5b51a0e664de8705e93b
aditya-arcot <dev@aarcot.com>
2025-12-27 15:23:13 -0600

    server - add field length validation

--- server/app/models/schemas/auth.py
+++ server/app/models/schemas/auth.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class RequestAccessRequest(BaseModel):
     email: EmailStr
-    first_name: str
-    last_name: str
+    first_name: str = Field(min_length=1, max_length=50)
+    last_name: str = Field(min_length=1, max_length=50)
 
 
 class RequestAccessResponse(BaseModel):
@@ -13,5 +13,5 @@ class RequestAccessResponse(BaseModel):
 
 
 class LoginRequest(BaseModel):
-    username: str
-    password: str
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=8, max_length=64)

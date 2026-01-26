d7bc36240216060d1b008e758daa6ce99a8d0f64
aditya-arcot <dev@aarcot.com>
2026-01-26 11:26:44 -0600

    server - move settings models

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -2,160 +2,24 @@ import os
 from pathlib import Path
 from typing import Annotated, Literal, Union
 
-from pydantic import BaseModel, Field, computed_field, model_validator
+from pydantic import Field, computed_field, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-class AdminSettings(BaseModel):
-    username: str
-    email: str
-    first_name: str
-    last_name: str
-    password: str
-
-
-class JWTSettings(BaseModel):
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-    refresh_token_expire_days: int
-
-
-class DatabaseSettings(BaseModel):
-    host: str
-    port: int
-    name: str
-    user: str
-    password: str
-
-    @computed_field
-    @property
-    def url(self) -> str:
-        return (
-            f"postgresql+asyncpg://{self.user}:"
-            f"{self.password}@{self.host}:"
-            f"{self.port}/{self.name}"
-        )
-
-
-class EmailSmtpSettings(BaseModel):
-    backend: Literal["smtp"]
-    # allow arbitrary string
-    email_from: str
-    smtp_host: str
-    smtp_username: str
-    smtp_password: str
-
-    @computed_field
-    @property
-    def smtp_port(self) -> int:
-        return 1025
-
-
-class EmailLocalSettings(BaseModel):
-    backend: Literal["local"]
-    email_from: str
-    smtp_host: str
-
-    @computed_field
-    @property
-    def smtp_port(self) -> int:
-        return 1026
-
-    @computed_field
-    @property
-    def smtp_username(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_password(self) -> None:
-        return None
-
-
-class EmailConsoleSettings(BaseModel):
-    backend: Literal["console"]
-
-    @computed_field
-    @property
-    def email_from(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_host(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_port(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_username(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_password(self) -> None:
-        return None
-
-
-class EmailDisabledSettings(BaseModel):
-    backend: Literal["disabled"]
-
-    @computed_field
-    @property
-    def email_from(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_host(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_port(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_username(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def smtp_password(self) -> None:
-        return None
-
+from app.models.schemas.config import (
+    AdminSettings,
+    DatabaseSettings,
+    EmailConsoleSettings,
+    EmailDisabledSettings,
+    EmailLocalSettings,
+    EmailSmtpSettings,
+    GitHubApiSettings,
+    GitHubConsoleSettings,
+    JWTSettings,
+)
 
 EmailSettings = Union[
     EmailSmtpSettings, EmailLocalSettings, EmailConsoleSettings, EmailDisabledSettings
 ]
-
-
-class GitHubApiSettings(BaseModel):
-    backend: Literal["api"]
-    repo_owner: str
-    token: str
-
-
-class GitHubConsoleSettings(BaseModel):
-    backend: Literal["console"]
-
-    @computed_field
-    @property
-    def repo_owner(self) -> None:
-        return None
-
-    @computed_field
-    @property
-    def token(self) -> None:
-        return None
-
-
 GitHubSettings = Union[GitHubApiSettings, GitHubConsoleSettings]
 
 
